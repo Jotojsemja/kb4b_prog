1a2d9e482a531e95863a71ebb4a365d3a9b55683
Jotojsemja <naprohlizecopera@gmail.com>
2025-09-25 10:10:44 +0200

    arrays

--- python/new.py
+++ python/new.py
@@ -19,13 +19,48 @@ def schody():
         print(i * znak)
     
 def validator_rod_cisla():
-    rodne = input("Zadej svoje rodné číslo, ucčitě si to neuložím: \n >" ) 
-    rok = int(rodne[0] + rodne[1])
-    if rok > 00 and rok < tento_rok:
-        print(rok)
+    while (True):
+        rodne = input("Zadej svoje rodné číslo, určitě si to neuložím: \n >" )
+        
+        mesic = int(rodne[2] + rodne[3])
+        den = int(rodne[4] + rodne[5])
+        if mesic < 63 and den < 32:
+            break
+        print("Chybička někde se vloudila")
+    # rok 
+    rok = rodne[0] + rodne[1]
+    if int(rok) < tento_rok+1:
+        rok_spravne = "20" + str(rok)
+    else:
+        rok_spravne = "19" + str(rok)
+    # měsíc
+    if mesic > 12:
+        mesic -= 50
+        pohlavi = "žena" 
+    else:
+        pohlavi = "muž"
+    # den 
+    
+
+    print(f"{den}.{mesic}.{rok_spravne} {pohlavi}")
+
+def validator_vstupu():
+    while(True):
+        zkontrolovat = input("Zadej vstup: ")
+        if (int(zkontrolovat) % 2 !=0):
+            print("tak ještě jednou")
+        else:
+            break
+
+#def vymazani_duplicit(pole):
+#    neduplikat = []
+#    for i in pole:
+#            if i == j:
+#                neduplikat.append(i)
+      
 cleaner()
 #vypsani_del()
 #schody()
-validator_rod_cisla()
-
+#validator_rod_cisla()
+validator_vstupu()
 
